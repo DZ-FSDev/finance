69da310a330de4577a2fcb1dfa9e17a3ef9e7d77
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-09-16 22:30:22 -0500

    Update FuturesOrder.java

--- futuresMarkets/domain/FuturesOrder.java
+++ futuresMarkets/domain/FuturesOrder.java
@@ -4,7 +4,10 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -17,14 +20,17 @@ import lombok.Setter;
  * Domain Object representing an Order placed in a futures market.
  * 
  * @version 0.0.1
- * @since 15.0.0.2
  * @author DZ-FSDev
+ * @since 16
  */
 @Entity
 public class FuturesOrder implements Serializable{
 	private static final long serialVersionUID = -8994718820789379908L;
 	
 	private @Getter @Setter @GeneratedValue @Id long orderId;
+	
+	public static enum OrderSide {LIMIT_SHORT, LIMIT_LONG, MARKET_SHORT, MARKET_LONG};
+	public static enum OrderStatus {NEW, ACTIVE, NSF, PARTIAL_FILL, FILLED, CANCELED, PARTIAL_CANCEL};
 	
 	private @Getter @Setter @ManyToOne(fetch = FetchType.LAZY, optional = false) FuturesAsset asset;
 	
@@ -34,10 +40,13 @@ public class FuturesOrder implements Serializable{
 	private @Getter @Setter BigDecimal strikePrice;
 	private @Getter @Setter BigDecimal premium;
 	
+	private @Getter @Setter @Enumerated(EnumType.ORDINAL) @Column(nullable = false, updatable = false) OrderSide orderSide;
+	private @Getter @Setter @Enumerated(EnumType.ORDINAL) @Column(nullable = false) OrderStatus orderStatus;
+	
 	private @Getter @Setter long expiry;
 	
 	/**
-	 * Default zero-arg constructor for Futures Order.
+	 * Default constructor for Futures Order.
 	 */
 	public FuturesOrder() {}
 
@@ -50,6 +59,8 @@ public class FuturesOrder implements Serializable{
 		result = prime * result + ((executedContracts == null) ? 0 : executedContracts.hashCode());
 		result = prime * result + (int) (expiry ^ (expiry >>> 32));
 		result = prime * result + (int) (orderId ^ (orderId >>> 32));
+		result = prime * result + ((orderSide == null) ? 0 : orderSide.hashCode());
+		result = prime * result + ((orderStatus == null) ? 0 : orderStatus.hashCode());
 		result = prime * result + ((premium == null) ? 0 : premium.hashCode());
 		result = prime * result + ((strikePrice == null) ? 0 : strikePrice.hashCode());
 		return result;
@@ -80,6 +91,10 @@ public class FuturesOrder implements Serializable{
 		if (expiry != other.expiry)
 			return false;
 		if (orderId != other.orderId)
+			return false;
+		if (orderSide != other.orderSide)
+			return false;
+		if (orderStatus != other.orderStatus)
 			return false;
 		if (premium == null) {
 			if (other.premium != null)
