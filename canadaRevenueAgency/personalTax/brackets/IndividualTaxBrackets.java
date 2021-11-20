c4922502f6c43c446fb4ff319cbce2e45529272b
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-11-19 19:58:50 -0600

    Create IndividualTaxBrackets.java

---
+++ canadaRevenueAgency/personalTax/brackets/IndividualTaxBrackets.java
@@ -0,0 +1,119 @@
+package com.dz_fs_dev.finance.canadaRevenueAgency.personalTax.brackets;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.dz_fs_dev.finance.canadaRevenueAgency.personalTax.brackets.interfaces.TaxBracketSet;
+
+/**
+ * Represents historical income tax brackets for individuals in Canada for the Provinces. Rates are
+ * laid out by the <a href="https://www.canada.ca/en/revenue-agency.html">Canada Revenue Agency</a>
+ * and updated annually.
+ * <p>The following tax years are supported:
+ * <ul>
+ * 	<li>2021</li>
+ * </ul>
+ * <p>The following tax brackets are supported:
+ * <ul>
+ * 	<li>Federal</li>
+ * 	<li>Provincial</li>
+ * 	<ul>
+ * 		<li>Newfoundland and Labrador</li>
+ * 		<li>Prince Edward Island</li>
+ * 	</ul>
+ * </ul>
+ * 
+ * @see <a href="https://laws-lois.justice.gc.ca/eng/acts/I-3.3/index.html">
+ *  	Income Tax Act (R.S.C., 1985, c.1 (5th Supp.))</a>.
+ * @author DZ_FSDev
+ * @since 16.0.1
+ * @version 0.0.2
+ */
+public enum IndividualTaxBrackets implements TaxBracketSet{
+	/** Canadian Federal Income Tax Rates: 2021 */
+	FEDERAL2021(new TaxBracket[] {
+			new TaxBracket(new BigDecimal("0.15"), BigDecimal.ZERO, new BigDecimal(49020)),
+			new TaxBracket(new BigDecimal("0.205"), new BigDecimal(49020), new BigDecimal(98040)),
+			new TaxBracket(new BigDecimal("0.26"), new BigDecimal(98040), new BigDecimal(151978)),
+			new TaxBracket(new BigDecimal("0.29"), new BigDecimal(151978), new BigDecimal(216511)),
+			new TaxBracket(new BigDecimal("0.33"), new BigDecimal(216511), null)
+	}),
+	
+	/** Newfoundland and Labrador Provincial Income Tax Rates: 2021 */
+	NL2021(new TaxBracket[] {
+			new TaxBracket(new BigDecimal("0.087"), BigDecimal.ZERO, new BigDecimal(38081)),
+			new TaxBracket(new BigDecimal("0.145"), new BigDecimal(38081), new BigDecimal(76161)),
+			new TaxBracket(new BigDecimal("0.158"), new BigDecimal(76161), new BigDecimal(135973)),
+			new TaxBracket(new BigDecimal("0.173"), new BigDecimal(151978), new BigDecimal(190363)),
+			new TaxBracket(new BigDecimal("0.183"), new BigDecimal(190363), null)
+	}),
+	
+	/** Prince Edward Island Provincial Income Tax Rates: 2021 */
+	PE2021(new TaxBracket[] {
+			new TaxBracket(new BigDecimal("0.098"), BigDecimal.ZERO, new BigDecimal(31984)),
+			new TaxBracket(new BigDecimal("0.138"), new BigDecimal(38081), new BigDecimal(63969)),
+			new TaxBracket(new BigDecimal("0.167"), new BigDecimal(63969), null)
+	});
+	
+	private final List<TaxBracket> TAX_BRACKETS;
+	
+	/**
+	 * Initializes a new set of Tax Brackets.
+	 * 
+	 * @param taxBrackets The set of tax brackets.
+	 */
+	private IndividualTaxBrackets(TaxBracket[] taxBrackets) {
+		this.TAX_BRACKETS = Collections.unmodifiableList(Arrays.asList(taxBrackets));
+	}
+
+	/**
+	 * @since 0.0.1
+	 */
+	@Override
+	public BigDecimal calculateTax(BigDecimal income) {
+		BigDecimal applicableTaxes = BigDecimal.ZERO;
+		
+		for(TaxBracket t : TAX_BRACKETS) {
+			income = income.subtract(t.getMAX_BRACKET_INCOME());
+			
+			BigDecimal taxableIncome = income.min(t.getBRACKET_INCOME_RANGE());
+			if(taxableIncome.compareTo(BigDecimal.ZERO) < 0)break;
+			applicableTaxes = applicableTaxes.add(taxableIncome.multiply(t.getTAX_RATE()));
+		}
+		
+		return applicableTaxes;
+	}
+	
+	/**
+	 * @since 0.0.2
+	 */
+	@Override
+	public BigDecimal calculateEffectiveTaxRate(BigDecimal income, int precision) {
+		// TODO
+		return income;
+	}
+	
+	/**
+	 * @since 0.0.2
+	 */
+	@Override
+	public BigDecimal compareEffectiveTaxRate(BigDecimal income, TaxBracketSet otherTaxBracket) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/**
+	 * Returns a link String representation of this set of tax brackets. 
+	 * 
+	 * @return 
+	 * @since 0.0.2
+	 */
+	@Override
+	public String toString() {
+		//TODO
+		return null;
+		
+	}
+}
