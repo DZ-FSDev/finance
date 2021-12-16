ff6bebe3d8cc25fd0ad039abd0fa955438ae217e
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-12-16 09:56:13 -0600

    Update SpotCandlestick.java

--- spotMarkets/domain/SpotCandlestick.java
+++ spotMarkets/domain/SpotCandlestick.java
@@ -6,6 +6,8 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -25,43 +27,54 @@ import lombok.Setter;
  * Domain Object for basic Candlesticks on Spot Markets
  * 
  * @author DZ-FSDev
- * @since 15.0.0.2
- * @version 0.0.1
+ * @since 17.0.1
+ * @version 0.0.2
  */
 @Entity
 public class SpotCandlestick implements Serializable{
-	private static final long serialVersionUID = 3048151457044300946L;
-	
+	/**
+	 * @since 0.0.2
+	 */
+	private static final long serialVersionUID = 1484395380636823816L;
+
 	private @Getter @Setter @Id @GeneratedValue long id;
 	
+	private @Getter @Setter BigDecimal askOpen;
+	private @Getter @Setter BigDecimal bidOpen;
+	private @Getter @Setter BigDecimal askClose;
+	private @Getter @Setter BigDecimal bidClose;
 	private @Getter @Setter BigDecimal open;
-	private @Getter @Setter BigDecimal close;
 	private @Getter @Setter BigDecimal high;
 	private @Getter @Setter BigDecimal low;
+	private @Getter @Setter BigDecimal close;
 	private @Getter @Setter BigDecimal quoteVolume;
 	private @Getter @Setter BigInteger assetVolume;
 
-	private @Getter @Setter Long startTimeStamp;
-	private @Getter @Setter Long endTimeStamp;
+	private @Getter @Setter Long openTimeStamp;
+	private @Getter @Setter Long closeTimeStamp;
 	
+	@ElementCollection(targetClass=SpotTrade.class)
 	private @Getter @Setter @OneToMany(fetch = FetchType.LAZY) @JsonIgnore List<SpotTrade> executedTrades;
 
 	/**
-	 * Zero-arg constructor for SpotCandlestick
+	 * Default constructor for SpotCandlestick.
 	 */
 	public SpotCandlestick() {}
 	
 	/**
-	 * Constructs a new SpotCandlestick that should follow a previously closed candlestick
+	 * Constructs a new SpotCandlestick that should follow a previously closed candlestick.
 	 * 
-	 * @param prev The previous candlestick to grab the start time and price to open the new candlestick
+	 * @param prev The previous candlestick to grab the start time and price to open the new candlestick.
+	 * @since 0.0.2
 	 */
 	public SpotCandlestick(SpotCandlestick prev) {
-		setStartTimeStamp(prev.getEndTimeStamp());
-		setClose(prev.getClose());
-		setOpen(getClose());
-		setHigh(getClose());
-		setLow(getClose());
+		setOpenTimeStamp(prev.getCloseTimeStamp());
+		setAskClose(prev.getAskClose());
+		setBidClose(prev.getBidClose());
+		setAskOpen(getAskClose());
+		setBidOpen(getBidClose());
+		setHigh(getAskClose());
+		setLow(getBidClose());
 	}
 	
 	/**
@@ -69,14 +82,15 @@ public class SpotCandlestick implements Serializable{
 	 * 
 	 * @param trade The Spot Trade to be registered to the candle.
 	 * @throws TradeException Exception when trade registration was unsuccessful.
+	 * @since 0.0.2
 	 */
 	public void registerTrade(SpotTrade trade) throws TradeException {
-		if(trade.getTransactTime() > startTimeStamp && endTimeStamp != 0 && trade.getTransactTime() < endTimeStamp)
+		if(trade.getTransactTime() > openTimeStamp && closeTimeStamp != 0 && trade.getTransactTime() < closeTimeStamp)
 			throw new TradeException("Failed to Register Trade " + trade.getTradeId() + " - registration out of current candle time stamp range.");
 		
-		setClose(trade.getMaker().getQuote());
-		if(getLow().compareTo(getClose()) > 0)setLow(getClose());
-		if(getHigh().compareTo(getClose()) < 0)setHigh(getClose());
+		setBidClose(trade.getMaker().getQuote());
+		if(getLow().compareTo(getAskClose()) > 0)setLow(getAskClose());
+		if(getHigh().compareTo(getAskClose()) < 0)setHigh(getAskClose());
 		setAssetVolume(getAssetVolume().add(trade.getTransactedAssetVolume()));
 		setQuoteVolume(getQuoteVolume().add(trade.getMaker().getQuote().multiply(new BigDecimal(trade.getTransactedAssetVolume()))));
 		
@@ -88,29 +102,38 @@ public class SpotCandlestick implements Serializable{
 	 * Closes the current candle. All subsequent trade registrations will fail on this candle.
 	 * 
 	 * @throws CandleClosedException Thrown if candle is already closed.
+	 * @since 0.0.2
 	 */
 	public void closeCandle() throws CandleClosedException {
-		if(getEndTimeStamp() != null && !getEndTimeStamp().equals(Long.valueOf(0)))throw new CandleClosedException("Candle " + getId() + " is already closed.");
-		setEndTimeStamp(System.currentTimeMillis());
+		if(getCloseTimeStamp() != null && !getCloseTimeStamp().equals(Long.valueOf(0)))throw new CandleClosedException("Candle " + getId() + " is already closed.");
+		setCloseTimeStamp(System.currentTimeMillis());
 	}
 	
+	/**
+	 * @since 0.0.2
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 171731;
 		int result = 1;
+		result = prime * result + ((askClose == null) ? 0 : askClose.hashCode());
+		result = prime * result + ((askOpen == null) ? 0 : askOpen.hashCode());
 		result = prime * result + ((assetVolume == null) ? 0 : assetVolume.hashCode());
-		result = prime * result + ((close == null) ? 0 : close.hashCode());
-		result = prime * result + ((endTimeStamp == null) ? 0 : endTimeStamp.hashCode());
+		result = prime * result + ((bidClose == null) ? 0 : bidClose.hashCode());
+		result = prime * result + ((bidOpen == null) ? 0 : bidOpen.hashCode());
+		result = prime * result + ((closeTimeStamp == null) ? 0 : closeTimeStamp.hashCode());
 		result = prime * result + ((executedTrades == null) ? 0 : executedTrades.hashCode());
 		result = prime * result + ((high == null) ? 0 : high.hashCode());
 		result = prime * result + (int) (id ^ (id >>> 32));
 		result = prime * result + ((low == null) ? 0 : low.hashCode());
-		result = prime * result + ((open == null) ? 0 : open.hashCode());
 		result = prime * result + ((quoteVolume == null) ? 0 : quoteVolume.hashCode());
-		result = prime * result + ((startTimeStamp == null) ? 0 : startTimeStamp.hashCode());
+		result = prime * result + ((openTimeStamp == null) ? 0 : openTimeStamp.hashCode());
 		return result;
 	}
 
+	/**
+	 * @since 0.0.2
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -118,20 +141,35 @@ public class SpotCandlestick implements Serializable{
 		if (!(obj instanceof SpotCandlestick))
 			return false;
 		SpotCandlestick other = (SpotCandlestick) obj;
+		if (askClose == null) {
+			if (other.askClose != null)
+				return false;
+		} else if (!askClose.equals(other.askClose))
+			return false;
+		if (askOpen == null) {
+			if (other.askOpen != null)
+				return false;
+		} else if (!askOpen.equals(other.askOpen))
+			return false;
 		if (assetVolume == null) {
 			if (other.assetVolume != null)
 				return false;
 		} else if (!assetVolume.equals(other.assetVolume))
 			return false;
-		if (close == null) {
-			if (other.close != null)
+		if (bidClose == null) {
+			if (other.bidClose != null)
 				return false;
-		} else if (!close.equals(other.close))
+		} else if (!bidClose.equals(other.bidClose))
 			return false;
-		if (endTimeStamp == null) {
-			if (other.endTimeStamp != null)
+		if (bidOpen == null) {
+			if (other.bidOpen != null)
 				return false;
-		} else if (!endTimeStamp.equals(other.endTimeStamp))
+		} else if (!bidOpen.equals(other.bidOpen))
+			return false;
+		if (closeTimeStamp == null) {
+			if (other.closeTimeStamp != null)
+				return false;
+		} else if (!closeTimeStamp.equals(other.closeTimeStamp))
 			return false;
 		if (executedTrades == null) {
 			if (other.executedTrades != null)
@@ -150,31 +188,16 @@ public class SpotCandlestick implements Serializable{
 				return false;
 		} else if (!low.equals(other.low))
 			return false;
-		if (open == null) {
-			if (other.open != null)
-				return false;
-		} else if (!open.equals(other.open))
-			return false;
 		if (quoteVolume == null) {
 			if (other.quoteVolume != null)
 				return false;
 		} else if (!quoteVolume.equals(other.quoteVolume))
 			return false;
-		if (startTimeStamp == null) {
-			if (other.startTimeStamp != null)
+		if (openTimeStamp == null) {
+			if (other.openTimeStamp != null)
 				return false;
-		} else if (!startTimeStamp.equals(other.startTimeStamp))
+		} else if (!openTimeStamp.equals(other.openTimeStamp))
 			return false;
 		return true;
-	}
-	
-	static SpotCandlestick fromJSON(String jStr) throws JsonMappingException, JsonProcessingException {
-		ObjectMapper mapper = new ObjectMapper();
-		return mapper.readValue(jStr.toString(), SpotCandlestick.class);
-	}
-	
-	static Optional<String> toJSONstr(SpotCandlestick a) throws JsonProcessingException{
-		ObjectMapper mapper = new ObjectMapper();
-		return Optional.ofNullable(mapper.writeValueAsString(a));
 	}
 }
