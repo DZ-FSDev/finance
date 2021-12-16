a76233fa764b413764c6a015d1000f448dfeae22
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-12-16 09:57:18 -0600

    Update SpotCandlestickRepository.java

--- spotMarkets/repository/SpotCandlestickRepository.java
+++ spotMarkets/repository/SpotCandlestickRepository.java
@@ -1,9 +1,11 @@
 package com.dz_fs_dev.finance.spotMarkets.repository;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dz_fs_dev.finance.spotMarkets.domain.SpotCandlestick;
 
@@ -11,9 +13,15 @@ import com.dz_fs_dev.finance.spotMarkets.domain.SpotCandlestick;
  * Persistence layer for Spot Candlestick entities.
  * 
  * @author DZ-FSDev
- * @version 0.0.1
- * @since 16.0.1
+ * @version 0.0.2
+ * @since 17.0.1
  */
 public interface SpotCandlestickRepository extends JpaRepository<SpotCandlestick, Long>{
-	Optional<SpotCandlestick> findByClose(BigInteger close);
+	Optional<SpotCandlestick> findByClose(BigDecimal close);
+	Optional<SpotCandlestick> findByOpen(BigDecimal close);
+	Optional<SpotCandlestick> findByHigh(BigDecimal close);
+	Optional<SpotCandlestick> findByLow(BigDecimal close);
+
+	Optional<SpotCandlestick> findByOpenTimeStampBetween(Long begin, Long end);
+	Optional<SpotCandlestick> findByCloseTimeStampBetween(Long begin, Long end);
 }
