57094b90169579cef4d080d58398f4f012bdf149
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-12-20 15:39:45 -0600

    Update SpotOrder.java

--- spotMarkets/spotOrder/SpotOrder.java
+++ spotMarkets/spotOrder/SpotOrder.java
@@ -3,8 +3,10 @@ package com.dz_fs_dev.finance.spotMarkets.spotOrder;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Set;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -12,8 +14,10 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.dz_fs_dev.finance.spotMarkets.spotAsset.SpotAsset;
+import com.dz_fs_dev.finance.spotMarkets.spotTrade.SpotTrade;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -22,17 +26,19 @@ import lombok.Setter;
  * Domain Object representing an Order placed in a spot market.
  * 
  * @author DZ-FSDev
- * @version 0.0.1
+ * @version 0.0.2
  * @since 17.0.1
  */
 @Entity
-public class SpotOrder implements Serializable{
+public class SpotOrder implements Serializable, Comparable<SpotOrder>{
 	/**
-	 * @since 0.0.1
+	 * @since 0.0.2
 	 */
-	private static final long serialVersionUID = -786004822888844302L;
+	private static final long serialVersionUID = -3163946031455909029L;
+
+	private @Getter @Setter @GeneratedValue @Id Long orderId;
 	
-	private @Getter @Setter @GeneratedValue @Id long orderId;
+	private @Getter @Setter Long ts;
 	
 	private @Getter @Setter BigInteger amount;
 	private @Getter @Setter BigInteger executedAmount;
@@ -43,11 +49,25 @@ public class SpotOrder implements Serializable{
 	
 	private @Getter @Setter @ManyToOne(fetch=FetchType.LAZY) SpotAsset asset;
 	
+	private @Getter @Setter @ElementCollection @OneToMany(fetch = FetchType.LAZY) Set<SpotTrade> trades;
+	
 	/**
 	 * Default constructor for SpotOrder.
 	 */
 	public SpotOrder() {}
 
+	/**
+	 * @since 0.0.2
+	 */
+	@Override
+	public int compareTo(SpotOrder order) {
+		if(this.equals(order))return 0;
+		return this.getTs().compareTo(order.getTs());
+	}
+	
+	/**
+	 * @since 0.0.2
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 1476204733;
@@ -59,9 +79,14 @@ public class SpotOrder implements Serializable{
 		result = prime * result + ((orderSide == null) ? 0 : orderSide.hashCode());
 		result = prime * result + ((orderStatus == null) ? 0 : orderStatus.hashCode());
 		result = prime * result + ((quote == null) ? 0 : quote.hashCode());
+		result = prime * result + ((trades == null) ? 0 : trades.hashCode());
+		result = prime * result + (int) (ts ^ (ts >>> 32));
 		return result;
 	}
 
+	/**
+	 * @since 0.0.2
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -94,6 +119,13 @@ public class SpotOrder implements Serializable{
 			if (other.quote != null)
 				return false;
 		} else if (!quote.equals(other.quote))
+			return false;
+		if (trades == null) {
+			if (other.trades != null)
+				return false;
+		} else if (!trades.equals(other.trades))
+			return false;
+		if (ts != other.ts)
 			return false;
 		return true;
 	}
