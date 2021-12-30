2cdc2c3a8040d6317186218431c1b4fd5a4920e6
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-12-30 12:42:08 -0600

    Update ICapitalCostAllowance.java

--- canadaRevenueAgency/capitalAssets/ICapitalCostAllowance.java
+++ canadaRevenueAgency/capitalAssets/ICapitalCostAllowance.java
@@ -1,3 +1,22 @@
+/*  Modifications Licensing Copyright
+ * 
+ *  Interface representation of capital cost allowance classes handling
+ *  asset pools.
+ *  Copyright (C) 2021  DZ-FSDev
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.dz_fs_dev.finance.canadaRevenueAgency.capitalAssets;
 
 import java.util.Date;
@@ -9,7 +28,7 @@ import java.util.Date;
  * 
  * @author DZ-FSDev
  * @since 17.0.1
- * @version 0.0.1
+ * @version 0.0.2
  * @see https://www.canada.ca/en/revenue-agency/services/tax/businesses/topics/sole-proprietorships-partnerships/report-business-income-expenses/claiming-capital-cost-allowance/classes.html
  */
 public interface ICapitalCostAllowance {
@@ -17,11 +36,23 @@ public interface ICapitalCostAllowance {
 	 * Returns true if an asset pool is eligible for consideration under this
 	 * capital cost allowance classification; false otherwise.
 	 * 
-	 * @param acquistionDate
-	 * @param filingDate
+	 * @param assetPool The specified asset pool to be considered.
 	 * @return True if an asset pool is eligible for consideration under this
 	 * 			capital cost allowance classification; false otherwise.
-	 * @since 0.0.1
+	 * @since 0.0.2
 	 */
-	public boolean isEligible(Date acquistionDate, Date filingDate);
+	public boolean isEligible(IAssetPool assetPool);
+	
+	/**
+	 * Returns true if an asset pool is eligible for consideration under this
+	 * capital cost allowance classification by date; false otherwise.
+	 * 
+	 * @param acquistionDate The specified date of acquisition for the capital
+	 *        asset.
+	 * @param filingDate The specified date of tax filing for the capital asset.
+	 * @return True if an asset pool is eligible for consideration under this
+	 * 			capital cost allowance classification; false otherwise.
+	 * @since 0.0.2
+	 */
+	public boolean isEligibleByDate(Date acquistionDate, Date filingDate);
 }
