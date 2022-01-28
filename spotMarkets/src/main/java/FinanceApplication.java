98719862d67b9d7279488b7daef9b3f8440f83ea
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2022-01-28 07:57:02 -0600

    Create FinanceApplication.java

---
+++ spotMarkets/src/main/java/FinanceApplication.java
@@ -0,0 +1,36 @@
+/*  Original Licensing Copyright
+ * 
+ *  Finance Spot Market Spring Boot Application.
+ *  Copyright (C) 2022  DZ-FSDev
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.dz_fs_dev.finance.spotMarkets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Finance Spot Market Spring Boot Application.
+ * 
+ * @author DZ_FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
+@SpringBootApplication
+public class FinanceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceApplication.class, args);
+	}
+}
