88d2511454fa544de4bc43241ea2267aefdf5221
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-03 10:18:00 -0600

    Create WebConfig.java

---
+++ liquidPoolMarkets/src/main/java/WebConfig.java
@@ -0,0 +1,47 @@
+/*  Original Licensing Copyright
+ * 
+ *  Finance Liquid Market Spring Boot MVC Configuration.
+ *  Copyright (C) 2022  DZ-FSDev
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.dz_fs_dev.finance.liquidPoolMarkets;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Finance Liquid Market Spring Boot MVC Configuration.
+ * 
+ * @author DZ_FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
+@Configuration
+@EnableAsync
+@ComponentScan(basePackages = { "com.dz_fs_dev.finance.liquidPoolMarkets" })
+public class WebConfig implements WebMvcConfigurer {
+	// TODO Converter not detected.
+	@Bean
+	public HttpMessageConverter<BufferedImage> createImageHttpMessageConverter() {
+	    return new BufferedImageHttpMessageConverter();
+	}
+}
