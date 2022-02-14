9972b9bace2ebf56a783c0bb6f26b7a5384279b4
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-14 09:34:38 -0600

    Add initial JPA queries

--- liquidPoolMarkets/src/main/java/liquidPoolMarket/MarketRepository.java
+++ liquidPoolMarkets/src/main/java/liquidPoolMarket/MarketRepository.java
@@ -1,21 +1,36 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolMarket;
 
-import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAsset.Asset;
 
 /**
  * Persistence layer for Liquid Market entities.
  * 
  * @author DZ-FSDev
- * @version 0.0.1
+ * @version 0.0.2
  * @since 17.0.1
  */
-public interface MarketRepository {
+public interface MarketRepository extends JpaRepository<Market, Long>{
 	/*
 	 *  -- Read --
 	 */
-	List<Market> findAll();
+	public Optional<Market> findByMarketId(Long marketId);
+	public Optional<Market> findByTicker(String ticker);
+	public Optional<Market> findByLeftAsset(Asset asset);
+	public Optional<Market> findByRightAssetOrderByTicker(Asset asset);
+	public Optional<Market> findByTickerLike(String pattern);
+	public Optional<Market> findByTickerContainsAllIgnoreCase(String name);
+	
+	/*
+	public List<Asset> findFirst3OrderByVolume(BigInteger units);
+	public List<Asset> findFirst3OrderByPrice(BigInteger units);
+	*/
 	
 	/*
 	 *  -- Update --
 	 */
+	public long deleteByTicker(String ticker);
 }
