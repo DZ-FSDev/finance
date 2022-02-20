883857b658ae3b8631cbfb82b909905d7e185fab
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-19 18:33:08 -0600

    Add delete endpoint

--- liquidPoolMarkets/src/main/java/liquidPoolAsset/WEBController_v1_liquid_assets.java
+++ liquidPoolMarkets/src/main/java/liquidPoolAsset/WEBController_v1_liquid_assets.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * 
  * @author DZ_FSDev
  * @since 17.0.1
- * @version 0.0.4
+ * @version 0.0.5
  */
 @Controller
 @RequestMapping("/liquid/assets")
@@ -21,6 +21,11 @@ public class WEBController_liquid_assets {
 	@Autowired
 	IAssetService assetSvc;
 	
+	/**
+	 * 
+	 * @param model
+	 * @return
+	 */
 	@GetMapping
 	public String get(Model model) {
 		model.addAttribute("assets", assetSvc.getAllAssets());
@@ -28,9 +33,28 @@ public class WEBController_liquid_assets {
 		return "/liquid/assets/index";
 	}
 	
+	/**
+	 * 
+	 * @param asset
+	 * @param model
+	 * @return
+	 */
 	@PostMapping("/create")
 	public String createAsset(Asset asset, Model model) {
 		assetSvc.add(asset);
+				
+		return get(model);
+	}
+	
+	/**
+	 * 
+	 * @param asset
+	 * @param model
+	 * @return
+	 */
+	@PostMapping("/delete")
+	public String deleteAsset(Asset asset, Model model) {
+		assetSvc.remove(asset);
 				
 		return get(model);
 	}
