a34e83de7ea510112cdfab0cbf486be0956717cb
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-20 23:19:03 -0600

    Create add(Market)

--- liquidPoolMarkets/src/main/java/liquidPoolMarket/IMarketService.java
+++ liquidPoolMarkets/src/main/java/liquidPoolMarket/IMarketService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
  * with an liquid pool market service.
  * 
  * @author DZ-FSDev
- * @version 0.0.1
+ * @version 0.0.2
  * @since 17.0.1
  */
 @Service
@@ -22,4 +22,14 @@ public interface IMarketService {
 	 * @since 0.0.1
 	 */
 	List<Market> getAllMarkets();
+
+	/**
+	 * Adds a specific market to be tracked by the liquid pool exchange;
+	 * returning true if successful.
+	 * 
+	 * @param market The market to be added.
+	 * @return True if the addition was successful.
+	 * @since 0.0.2
+	 */
+	boolean add(Market market);
 }
