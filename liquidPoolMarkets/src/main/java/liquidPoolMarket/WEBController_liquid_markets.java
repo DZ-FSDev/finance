bf6cecafa50eb50a9b3c1d6c09b5c02d10a7b8c5
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-20 23:21:57 -0600

    Create WEBController_liquid_markets.java

---
+++ liquidPoolMarkets/src/main/java/liquidPoolMarket/WEBController_liquid_markets.java
@@ -0,0 +1,25 @@
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolMarket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * liquid/markets WEB Service Controller.
+ * 
+ * @author DZ_FSDev
+ * @since 17.0.1
+ * @version 0.0.7
+ */
+@Controller
+@RequestMapping("/liquid/markets")
+public class WEBController_liquid_markets {
+	@Autowired
+	IAssetService assetSvc;
+}
