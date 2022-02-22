55290ea97a46a36f40220bd930960d7dae018253
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-22 14:03:56 -0600

    Add read by dynamic projection

--- liquidPoolMarkets/src/main/java/liquidPoolMarket/MarketRepository.java
+++ liquidPoolMarkets/src/main/java/liquidPoolMarket/MarketRepository.java
@@ -1,5 +1,6 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolMarket;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +11,7 @@ import com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAsset.Asset;
  * Persistence layer for Liquid Market entities.
  * 
  * @author DZ-FSDev
- * @version 0.0.2
+ * @version 0.0.3
  * @since 17.0.1
  */
 public interface MarketRepository extends JpaRepository<Market, Long>{
@@ -23,6 +24,8 @@ public interface MarketRepository extends JpaRepository<Market, Long>{
 	public Optional<Market> findByRightAssetOrderByTicker(Asset asset);
 	public Optional<Market> findByTickerLike(String pattern);
 	public Optional<Market> findByTickerContainsAllIgnoreCase(String name);
+	
+	public <T> List<T> findAllByOrderByTicker(Class<T> type);
 	
 	/*
 	public List<Asset> findFirst3OrderByVolume(BigInteger units);
