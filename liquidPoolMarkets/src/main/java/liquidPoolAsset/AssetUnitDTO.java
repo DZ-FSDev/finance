dd26b35a360033896145fcf54e00ff38f6317424
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-22 22:36:45 -0600

    Update AssetUnitDTO.java

--- liquidPoolMarkets/src/main/java/liquidPoolAsset/AssetUnitDTO.java
+++ liquidPoolMarkets/src/main/java/liquidPoolAsset/AssetUnitDTO.java
@@ -5,22 +5,29 @@ import java.math.BigInteger;
 import lombok.Getter;
 
 /**
- * 
+ * Represents a liquid {@link Asset} class-based projection containing the
+ * units and string identifiers.
  * 
  * @author DZ-FSDev
  * @since 17.0.1
- * @version 0.0.1
+ * @version 0.0.2
  */
 public class AssetUnitDTO {
 	private @Getter String name;
+	private @Getter String symbol;
 	private @Getter BigInteger units;
 	
 	/**
+	 * Initializes an instance of the AssetUnitDTO with a specified name,
+	 * symbol and units within the liquid pool.
+	 * 
 	 * @param name
 	 * @param units
+	 * @param symbol
 	 */
-	public AssetUnitDTO(String name, BigInteger units) {
+	public AssetUnitDTO(String name, String symbol, BigInteger units) {
 		this.name = name;
+		this.symbol = symbol;
 		this.units = units;
 	}
 }
