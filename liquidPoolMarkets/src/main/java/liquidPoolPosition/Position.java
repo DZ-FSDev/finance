48c7f9eaac6035f45f70ca3aec31ea878121699e
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-23 23:57:28 -0600

    Add hashCode() & equals()

--- liquidPoolMarkets/src/main/java/liquidPoolPosition/Position.java
+++ liquidPoolMarkets/src/main/java/liquidPoolPosition/Position.java
@@ -19,7 +19,7 @@ import lombok.Setter;
  * 
  * @author DZ-FSDev
  * @since 17.0.1
- * @version 0.0.1
+ * @version 0.0.2
  */
 @Entity
 public class Position implements Serializable{
@@ -33,4 +33,47 @@ public class Position implements Serializable{
 	private @Getter @Setter @ManyToOne(fetch = FetchType.LAZY, optional = false) Account account;
 	
 	private @Getter @Setter BigDecimal costBasis;
+
+	/**
+	 * @since 0.0.2
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((account == null) ? 0 : account.hashCode());
+		result = prime * result + ((costBasis == null) ? 0 : costBasis.hashCode());
+		result = prime * result + ((positionId == null) ? 0 : positionId.hashCode());
+		return result;
+	}
+
+	/**
+	 * @since 0.0.2
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Position))
+			return false;
+		Position other = (Position) obj;
+		if (account == null) {
+			if (other.account != null)
+				return false;
+		} else if (!account.equals(other.account))
+			return false;
+		if (costBasis == null) {
+			if (other.costBasis != null)
+				return false;
+		} else if (!costBasis.equals(other.costBasis))
+			return false;
+		if (positionId == null) {
+			if (other.positionId != null)
+				return false;
+		} else if (!positionId.equals(other.positionId))
+			return false;
+		return true;
+	}
+	
+	
 }
