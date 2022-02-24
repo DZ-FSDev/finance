a4c486fa2a7e606e18ecf6c20c0c08604fa1e466
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-23 23:49:21 -0600

    Create IPositionService.java

---
+++ liquidPoolMarkets/src/main/java/liquidPoolMarkets/IPositionService.java
@@ -0,0 +1,16 @@
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolPosition;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * The user of this interface should implement the various actions associated
+ * with a liquid pool position services.
+ * 
+ * @author DZ-FSDev
+ * @version 0.0.1
+ * @since 17.0.1
+ */
+@Service
+public interface IPositionService {
+
+}
