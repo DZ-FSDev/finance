39efa8362519b6794a411923e2e8bf9489ee5e6c
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-25 12:53:46 -0600

    Update documentation for MarketSimpleDTO

--- liquidPoolMarkets/src/main/java/liquidPoolMarket/MarketSimpleDTO.java
+++ liquidPoolMarkets/src/main/java/liquidPoolMarket/MarketSimpleDTO.java
@@ -5,6 +5,9 @@ import com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAsset.Asset;
 import lombok.Getter;
 
 /**
+ * Represents a simple read-only class-based projection of the liquid pool
+ * {@link Market} entity. Contains a the name of the left and right assets
+ * along with the ticker.
  * 
  * @author DZ-FSDev
  * @since 17.0.1
@@ -17,6 +20,15 @@ public class MarketSimpleDTO {
 	
 	private @Getter String ticker;
 
+	/**
+	 * Initializes a new instance of a {@link MarketSimpleDTO} which contains
+	 * the name of the left and right asset as well as the ticker for the
+	 * liquid pool {@link Market} entity it represents.
+	 * 
+	 * @param leftAsset The name of the left asset in this liquid pool market.
+	 * @param rightAsset The name of the right asset in this liquid pool market.
+	 * @param ticker The ticker of the market.
+	 */
 	public MarketSimpleDTO(Asset leftAsset, Asset rightAsset, String ticker) {
 		this.leftAsset = leftAsset.getName();
 		this.rightAsset = rightAsset.getName();
