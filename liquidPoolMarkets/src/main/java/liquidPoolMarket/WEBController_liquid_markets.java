a12930cb3b96204a08a3c31918c39053d3ecd557
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-25 12:25:46 -0600

    Add Copyright; update getChartCSV

--- liquidPoolMarkets/src/main/java/liquidPoolMarket/WEBController_liquid_markets.java
+++ liquidPoolMarkets/src/main/java/liquidPoolMarket/WEBController_liquid_markets.java
@@ -1,3 +1,21 @@
+/*  Original Licensing Copyright
+ * 
+ *  liquid/markets WEB Service Controller.
+ *  Copyright (C) 2022  DZ-FSDev
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolMarket;
 
 import java.io.IOException;
@@ -24,7 +42,7 @@ import org.springframework.web.servlet.ModelAndView;
  * 
  * @author DZ_FSDev
  * @since 17.0.2
- * @version 0.0.9
+ * @version 0.0.10
  */
 @Controller
 @RequestMapping("/liquid/markets")
@@ -119,14 +137,14 @@ public class WEBController_liquid_markets {
 	 * 
 	 * @param ticker
 	 * @param response
-	 * @since 0.0.9
+	 * @since 0.0.10
 	 */
 	@GetMapping(value = "/chartcsv/{ticker}")
 	@ResponseBody
 	public void getChartCSV(@PathVariable String ticker, HttpServletResponse response){
 		response.setContentType("text/csv");
 		try (Writer writer = new OutputStreamWriter(response.getOutputStream(), StandardCharsets.UTF_8)) {
-			writer.write(marketSvc.getChart());
+			writer.write(marketSvc.getChartCSV(ticker));
 			writer.flush();
 		}catch (IOException e) {
 			// TODO Auto-generated catch block
