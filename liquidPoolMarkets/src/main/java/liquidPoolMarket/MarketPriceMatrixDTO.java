3f75b2eb1b5e26ccba6b73b5928e61b5df2e5394
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-25 15:02:23 -0600

    Update MarketPriceMatrixDTO.java

--- liquidPoolMarkets/src/main/java/liquidPoolMarket/MarketPriceMatrixDTO.java
+++ liquidPoolMarkets/src/main/java/liquidPoolMarket/MarketPriceMatrixDTO.java
@@ -1,5 +1,11 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolMarket;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAsset.AssetUnitDTO;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,12 +14,37 @@ import lombok.Setter;
  * 
  * @author DZ_FSDev
  * @since 17.0.1
- * @version 0.0.1
+ * @version 0.0.3
  */
 public class MarketPriceMatrixDTO {
 	static MarketPriceMatrixDTO cachedInstance;
 	
 	private @Getter @Setter long timestamp;
 	
+	private @Getter @Setter List<AssetUnitDTO> currencies;
+	private @Getter @Setter List<AssetUnitDTO> assets;
 	
+	private @Getter @Setter BigDecimal[][] prices;
+
+	/**
+	 * @param currencies
+	 * @param assets
+	 * @param prices
+	 */
+	public MarketPriceMatrixDTO(List<AssetUnitDTO> currencies, List<AssetUnitDTO> assets, BigDecimal[][] prices) {
+		this.currencies = currencies;
+		this.assets = assets;
+		this.prices = prices;
+		
+		this.timestamp = System.currentTimeMillis();
+	}
+
+	/**
+	 * @since 0.0.3
+	 */
+	@Override
+	public String toString() {
+		return "MarketPriceMatrixDTO [timestamp=" + timestamp + ", currencies=" + currencies + ", otherAssets="
+				+ assets + ", prices=" + Arrays.toString(prices) + "]";
+	}
 }
