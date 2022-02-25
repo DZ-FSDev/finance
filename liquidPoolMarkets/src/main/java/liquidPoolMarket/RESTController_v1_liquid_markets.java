0656be8b4e7b7c943b637a614e798382e49335d0
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-25 15:18:56 -0600

    Remove get() endpoint

--- liquidPoolMarkets/src/main/java/liquidPoolMarket/RESTController_v1_liquid_markets.java
+++ liquidPoolMarkets/src/main/java/liquidPoolMarket/RESTController_v1_liquid_markets.java
@@ -1,6 +1,7 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolMarket;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
  * v1/liquid/markets REST Service Controller.
  * 
  * @author DZ_FSDev
- * @since 17.0.1
- * @version 0.0.1
+ * @since 17.0.2
+ * @version 0.0.2
  */
 @RestController
 @RequestMapping("/v1/liquid/markets")
@@ -19,13 +20,8 @@ public class RESTController_v1_liquid_markets {
 	@Autowired
 	IMarketService marketSvc;
 	
-	@GetMapping
-	public String get() {
-		return "Hello Markets";
-	}
-	
 	@GetMapping(value = "/list")
-	public List<Market> getAllAssets(){
+	public List<Market> getAllMarkets(){
 		return marketSvc.getAllMarkets();
 	}
 }
