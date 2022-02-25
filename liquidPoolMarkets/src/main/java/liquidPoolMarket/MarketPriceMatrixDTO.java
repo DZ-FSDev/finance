50f1f4d6337072d38a1b349cd9f55a91b68232e9
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-25 15:16:23 -0600

    Update documentation

--- liquidPoolMarkets/src/main/java/liquidPoolMarket/MarketPriceMatrixDTO.java
+++ liquidPoolMarkets/src/main/java/liquidPoolMarket/MarketPriceMatrixDTO.java
@@ -1,3 +1,21 @@
+/*  Original Licensing Copyright
+ * 
+ *  Represents a matrix of prices in a liquid pool market.
+ *  Copyright (C) 2022  DZ-FSDev
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolMarket;
 
 import java.math.BigDecimal;
@@ -9,7 +27,7 @@ import com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAsset.AssetUnitDTO;
 import lombok.Getter;
 
 /**
- * Represents a matrix of prices that exist in the current liquid pool market.
+ * Represents a matrix of prices that exist in a liquid pool market.
  * 
  * @author DZ_FSDev
  * @since 17.0.1
@@ -28,9 +46,9 @@ public class MarketPriceMatrixDTO {
 	/**
 	 * Initializes a new instance of a matrix of prices that exist in the current liquid pool market.
 	 * 
-	 * @param currencies
-	 * @param assets
-	 * @param prices
+	 * @param currencies The list of currencies in the price matrix.
+	 * @param assets The list of assets priced against the currencies in the price matrix.
+	 * @param prices The 2D array of prices that map between the currencies and assets.
 	 */
 	public MarketPriceMatrixDTO(List<AssetUnitDTO> currencies, List<AssetUnitDTO> assets, BigDecimal[][] prices) {
 		this.currencies = currencies;
