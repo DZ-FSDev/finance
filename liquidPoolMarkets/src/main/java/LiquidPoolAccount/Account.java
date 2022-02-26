53cac61e961eec72f6587443257e47a4e638fa99
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-26 10:48:19 -0600

    Add name and List<Position> fields & constraints

--- liquidPoolMarkets/src/main/java/LiquidPoolAccount/Account.java
+++ liquidPoolMarkets/src/main/java/LiquidPoolAccount/Account.java
@@ -1,10 +1,14 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAccount;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolPosition.Position;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +18,7 @@ import lombok.Setter;
  * 
  * @author DZ-FSDev
  * @since 17.0.1
- * @version 0.0.1
+ * @version 0.0.3
  */
 @Entity
 public class Account implements Serializable{
@@ -23,6 +27,9 @@ public class Account implements Serializable{
 	 */
 	private static final long serialVersionUID = 8539911094908576689L;
 
-	private @Getter @Setter @Id @GeneratedValue Long accountId;
+	private @Getter @Setter @Id @GeneratedValue Long id;
 	
+	private @Getter @Setter @Id @GeneratedValue String name;
+	
+	private @Getter @Setter @OneToMany(fetch = FetchType.LAZY) List<Position> positions;
 }
