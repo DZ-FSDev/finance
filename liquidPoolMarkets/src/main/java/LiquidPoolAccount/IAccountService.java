0cb366a2f9e55ee499226095ed1016b414131704
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-26 10:49:12 -0600

    Create IAccountService.java

---
+++ liquidPoolMarkets/src/main/java/LiquidPoolAccount/IAccountService.java
@@ -0,0 +1,16 @@
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAccount;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * The user of this interface should implement the various actions associated
+ * with an liquid pool account service.
+ * 
+ * @author DZ-FSDev
+ * @since 17.0.2
+ * @version 0.0.1
+ */
+@Service
+public interface IAccountService {
+	
+}
