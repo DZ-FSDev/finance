4ee6f5fe2aa4f1c28e093242b061116d1e1c3827
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-28 16:37:50 -0600

    Create RESTController_v1_liquid_accounts.java

---
+++ liquidPoolMarkets/src/main/java/LiquidPoolAccount/RESTController_v1_liquid_accounts.java
@@ -0,0 +1,17 @@
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAccount;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * v1/liquid/accounts REST Service Controller.
+ * 
+ * @author DZ-FSDev
+ * @version 17.0.2
+ * @since 0.0.1
+ */
+@RestController
+@RequestMapping("/v1/liquid/accounts")
+public class RESTController_v1_liquid_accounts {
+
+}
