9e178fc292da8d40339e2fea52755953454d180c
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-28 15:52:24 -0600

    Add additional JPA queries

--- liquidPoolMarkets/src/main/java/LiquidPoolAccount/IAccountRepository.java
+++ liquidPoolMarkets/src/main/java/LiquidPoolAccount/IAccountRepository.java
@@ -1,21 +1,31 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAccount;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolOrder.LiquidOrder;
+import com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolPosition.Position;
 
 /**
  * Persistence layer for Liquid Account entities.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.2
+ * @version 0.0.3
  */
 public interface IAccountRepository extends JpaRepository<Account, Long> {
 	/*
 	 *  -- Read --
 	 */
 	public Optional<Account> findByName(String name);
+	
+	public List<Position> findPositionsByName(String name);
+	
+	public List<LiquidOrder> findOrdersByName(String name);
+	
+	public <T> List<T> findFirst3ByOrderByRealizedPNL();
 	
 	/*
 	 *  -- Delete --
