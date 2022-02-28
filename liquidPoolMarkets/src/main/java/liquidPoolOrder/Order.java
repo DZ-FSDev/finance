08f9a4c80d08190c7164dcf279302fd40f78fa86
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-02-28 07:59:55 -0600

    Update Order.java

--- liquidPoolMarkets/src/main/java/liquidPoolOrder/Order.java
+++ liquidPoolMarkets/src/main/java/liquidPoolOrder/Order.java
@@ -1,5 +1,6 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolOrder;
 
+import java.io.Serializable;
 import java.math.BigInteger;
 
 import javax.persistence.Entity;
@@ -9,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 import com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAccount.Account;
+import com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAsset.Asset;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -18,15 +21,22 @@ import lombok.Setter;
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.1
+ * @version 0.0.3
  */
 @Entity
-public class Order {
+public class Order implements Serializable{
+	/**
+	 * @since 0.0.3
+	 */
+	private static final long serialVersionUID = -2414199150565231642L;
+
 	private @Getter @Setter @GeneratedValue @Id Long id;
 	
 	private @Getter @Setter Long timestamp;
 	
+	private @Getter @Setter @ManyToOne(fetch = FetchType.LAZY) Asset asset;
 	private @Getter @Setter BigInteger units;
+	private @Getter @Setter BigInteger allowableCostBasis;
 	
-	private @Getter @Setter @ManyToOne(fetch = FetchType.LAZY) Account account;
+	private @Getter @Setter @JsonIgnore @ManyToOne(fetch = FetchType.LAZY) Account account;
 }
