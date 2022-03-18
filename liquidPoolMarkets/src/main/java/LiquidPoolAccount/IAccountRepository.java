17a64f8fa0afca8539b27a3d00713fe5fd080ef2
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-18 14:57:00 -0500

    impl. ReactiveCrudRepository<Account, Long>

--- liquidPoolMarkets/src/main/java/LiquidPoolAccount/IAccountRepository.java
+++ liquidPoolMarkets/src/main/java/LiquidPoolAccount/IAccountRepository.java
@@ -1,37 +1,30 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAccount;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolOrder.LiquidOrder;
-import com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolPosition.Position;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Persistence layer for Liquid Account entities.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.5
+ * @version 0.0.6
  */
-public interface IAccountRepository extends JpaRepository<Account, Long> {
+public interface IAccountRepository extends ReactiveCrudRepository<Account, Long> {
 	/*
 	 *  -- Read --
 	 */
-	public Optional<Account> findByName(String name);
+	public Mono<Account> findByName(String name);
 	
-	public List<Position> findPositionsByName(String name);
-	
-	public List<LiquidOrder> findOrdersByName(String name);
-	
-	public <T> List<T> findFirst3ByOrderByRealizedPNL(Class<T> type);
-	public <T> List<T> findFirst5ByOrderByRealizedPNL(Class<T> type);
-	public <T> List<T> findFirst10ByOrderByRealizedPNL(Class<T> type);
-	public <T> List<T> findFirst100ByOrderByRealizedPNL(Class<T> type);
+	public <T> Flux<T> findFirst3ByOrderByRealizedPNL(Class<T> type);
+	public <T> Flux<T> findFirst5ByOrderByRealizedPNL(Class<T> type);
+	public <T> Flux<T> findFirst10ByOrderByRealizedPNL(Class<T> type);
+	public <T> Flux<T> findFirst100ByOrderByRealizedPNL(Class<T> type);
 	
 	/*
 	 *  -- Delete --
 	 */
-	public long deleteByName(String name);
+	public Mono<Boolean> deleteByName(String name);
 }
