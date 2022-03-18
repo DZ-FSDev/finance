7f094c17818094aa8dc2fe3f7b35817da1999ce9
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-18 15:05:30 -0500

    Update IAccountService.java

--- liquidPoolMarkets/src/main/java/LiquidPoolAccount/IAccountService.java
+++ liquidPoolMarkets/src/main/java/LiquidPoolAccount/IAccountService.java
@@ -2,15 +2,21 @@ package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAccount;
 
 import org.springframework.stereotype.Service;
 
+import com.dz_fs_dev.finance.markets.interfaces.IAccount;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 /**
  * The user of this interface should implement the various actions associated
  * with an liquid pool account service.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.1
+ * @version 0.0.2
  */
 @Service
 public interface IAccountService {
-	
+	public <T extends IAccount> Mono<T> findByName(Class<T> type, String name);
+	public <T> Flux<T> findTopXByOrderByRealizedPNL(Class<T> type, int x);
 }
