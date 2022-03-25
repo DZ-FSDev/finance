01cd6c2e4597fbfa44cc23900d75fb5724d3d0a8
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-24 22:54:37 -0500

    Disable endpoints

--- liquidPoolMarkets/src/main/java/liquidPoolAsset/WEBController_v1_liquid_assets.java
+++ liquidPoolMarkets/src/main/java/liquidPoolAsset/WEBController_v1_liquid_assets.java
@@ -3,22 +3,15 @@ package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAsset;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * liquid/assets WEB Service Controller.
  * 
  * @author DZ_FSDev
- * @since 17.0.1
- * @version 0.0.7
+ * @since 17.0.2
+ * @version 0.0.8
  */
 @Controller
 @RequestMapping("/liquid/assets")
@@ -35,6 +28,7 @@ public class WEBController_liquid_assets {
 	@GetMapping
 	public String get(Model model) {
 		model.addAttribute("assets", assetSvc.getAllAssets());
+		model.addAttribute("assetClasses", AssetClass.values());
 		model.addAttribute("updDTO", new AssetUpdateDTO());
 		
 		return "/liquid/assets/index";
@@ -46,7 +40,7 @@ public class WEBController_liquid_assets {
 	 * @param model
 	 * @return
 	 * @since 0.0.7
-	 */
+	 *
 	@PostMapping("/create")
 	public ModelAndView createAsset(Asset asset, ModelMap model) {
 		assetSvc.add(asset);
@@ -60,7 +54,7 @@ public class WEBController_liquid_assets {
 	 * @param model
 	 * @return
 	 * @since 0.0.7
-	 */
+	 *
 	@PostMapping("/delete")
 	public ModelAndView deleteAsset(Asset asset, ModelMap model) {
 		assetSvc.remove(asset);
@@ -74,11 +68,12 @@ public class WEBController_liquid_assets {
 	 * @param model
 	 * @return
 	 * @since 0.0.7
-	 */
-	@PostMapping("/update")//
+	 *
+	@PostMapping("/update")
 	public ModelAndView updateAsset(@ModelAttribute("updDTO") AssetUpdateDTO updDTO, ModelMap model) {
 		assetSvc.update(updDTO.from, updDTO.to);
 				
 		return new ModelAndView("redirect:/liquid/assets", model);
 	}
+	*/
 }
