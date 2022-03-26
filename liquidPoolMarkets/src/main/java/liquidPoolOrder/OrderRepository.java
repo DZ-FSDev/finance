36b9679081f4194aa8520e4be798b7820d4a09c6
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-26 15:40:20 -0500

    Create OrderRepository.java

---
+++ liquidPoolMarkets/src/main/java/liquidPoolOrder/OrderRepository.java
@@ -0,0 +1,20 @@
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolOrder;
+
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Persistence layer for Liquid Order entities.
+ * 
+ * @author DZ-FSDev
+ * @since 17.0.2
+ * @version 0.0.1
+ */
+public interface OrderRepository extends R2dbcRepository<LiquidOrder, Long>{
+	/*
+	 *  -- Read --
+	 */
+	
+	/*
+	 *  -- Update --
+	 */
+}
