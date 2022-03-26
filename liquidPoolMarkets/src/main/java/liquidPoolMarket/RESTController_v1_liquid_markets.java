227c72ae84541fb8f17b9ebd00b6730985713fab
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-25 21:03:03 -0500

    Update RESTController_v1_liquid_markets.java

--- liquidPoolMarkets/src/main/java/liquidPoolMarket/RESTController_v1_liquid_markets.java
+++ liquidPoolMarkets/src/main/java/liquidPoolMarket/RESTController_v1_liquid_markets.java
@@ -1,18 +1,18 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolMarket;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
 
 /**
  * v1/liquid/markets REST Service Controller.
  * 
  * @author DZ_FSDev
  * @since 17.0.2
- * @version 0.0.2
+ * @version 0.0.3
  */
 @RestController
 @RequestMapping("/v1/liquid/markets")
@@ -21,7 +21,7 @@ public class RESTController_v1_liquid_markets {
 	IMarketService marketSvc;
 	
 	@GetMapping(value = "/list")
-	public List<Market> getAllMarkets(){
+	public Flux<Market> getAllMarkets(){
 		return marketSvc.getAllMarkets();
 	}
 }
