bcb2928086b70f277290674f6ccd52fa101a9a95
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-26 22:39:15 -0500

    Update PositionRepository.java

--- liquidPoolMarkets/src/main/java/liquidPoolPosition/PositionRepository.java
+++ liquidPoolMarkets/src/main/java/liquidPoolPosition/PositionRepository.java
@@ -1,16 +1,23 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolPosition;
 
-import java.util.List;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import reactor.core.publisher.Flux;
 
 /**
  * Persistence layer for Liquid Position entities.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.1
+ * @version 0.0.2
  */
-public interface PositionRepository extends JpaRepository<Position, Long>{
-	public <T> List<T> findAllBy(Class<T> type);
+public interface PositionRepository extends R2dbcRepository<Position, Long>{
+	/*
+	 *  -- Read --
+	 */
+	public <T> Flux<T> findAllBy(Class<T> type);
+	
+	/*
+	 *  -- Delete --
+	 */
 }
