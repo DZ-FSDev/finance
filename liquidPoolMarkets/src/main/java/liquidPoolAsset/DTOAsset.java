194e9ee71d85e6a8d6586d814fed395b9c7f9ac8
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-27 12:24:41 -0500

    Update DTOAsset.java

--- liquidPoolMarkets/src/main/java/liquidPoolAsset/DTOAsset.java
+++ liquidPoolMarkets/src/main/java/liquidPoolAsset/DTOAsset.java
@@ -1,6 +1,8 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAsset;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +13,11 @@ import lombok.ToString;
  * Represents an Asset in liquid pool markets.
  * 
  * @author DZ-FSDev
- * @version 0.0.1
+ * @version 0.0.2
  * @since 17.0.2
  */
 @Data
+@Table("asset")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
