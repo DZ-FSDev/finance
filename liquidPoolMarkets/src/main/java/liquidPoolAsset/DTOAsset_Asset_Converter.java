ff56014718938f1832e4e82c2490576477548dd7
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-03-27 12:03:22 -0500

    Update DTOAsset_Asset_Converter.java

--- liquidPoolMarkets/src/main/java/liquidPoolAsset/DTOAsset_Asset_Converter.java
+++ liquidPoolMarkets/src/main/java/liquidPoolAsset/DTOAsset_Asset_Converter.java
@@ -1,9 +1,12 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAsset;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 import com.dz_fs_dev.finance.liquidPoolMarkets.Converter;
 
 /**
- * 
+ * Converts between {@link DTOAsset} and {@link Asset}.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
@@ -16,10 +19,10 @@ public class DTOAsset_Asset_Converter extends Converter<DTOAsset, Asset> {
 	}
 	
 	public static DTOAsset convertToDTO(Asset asset) {
-		return null;
+		return new DTOAsset(asset.getId(), asset.getSymbol(), asset.getName(), asset.getUnits().toString(), asset.getLastPrice().toString(), asset.getAssetClass());
 	}
 	
 	public static Asset convertToEntity(DTOAsset asset) {
-		return null;
+		return new Asset(asset.getId(), asset.getSymbol(), asset.getName(), new BigInteger(asset.getUnits()), new BigDecimal(asset.getLastPrice()), asset.getAssetClass());
 	}
 }
