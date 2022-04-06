e353ec1ba42e76a83a7f6360037d215615eb1bda
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-05 23:03:51 -0500

    Update IAccountRepository.java

--- liquidPoolMarkets/src/main/java/LiquidPoolAccount/IAccountRepository.java
+++ liquidPoolMarkets/src/main/java/LiquidPoolAccount/IAccountRepository.java
@@ -28,7 +28,7 @@ import reactor.core.publisher.Mono;
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.14
+ * @version 0.0.15
  */
 public interface IAccountRepository extends ReactiveCrudRepository<DTOAccount, Long> {
 	/*
@@ -37,6 +37,7 @@ public interface IAccountRepository extends ReactiveCrudRepository<DTOAccount, L
 	<T extends IAccount> Mono<T> findByFirstNameAndLastName(Class<T> type, String firstName, String lastName);
 	
 	<T extends IAccount> Flux<T> findByFirstNameLike(Class<T> type, String pattern);
+	<T extends IAccount> Flux<T> findByLastNameLike(Class<T> type, String pattern);
 	
 	
 	<T extends IAccount> Flux<T> findFirst3ByOrderByRealizedPNL(Class<T> type);
