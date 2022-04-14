71b4db06b2d2a6a69dfea86f1a58ff2ce113c1a7
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-14 14:54:09 -0500

    Create DTOLiquidOrder.java

---
+++ liquidPoolMarkets/src/main/java/liquidPoolOrder/DTOLiquidOrder.java
@@ -0,0 +1,34 @@
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolOrder;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Represents an Order placed in a liquid pool market.
+ * 
+ * @author DZ-FSDev
+ * @since 17.0.2
+ * @version 0.0.1
+ */
+@Data
+@Table("liquidOrder")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class DTOLiquidOrder implements ILiquidOrder{
+	private @Id Long id;
+	
+	private Long timestamp;
+	private String units;
+	private String allowableCostBasis;
+	
+	private Long assetId;
+	private Long accountId;
+}
