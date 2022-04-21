99bbb5b67ec191ed52b53d12bb1430ad7ff2ba2d
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-21 09:51:31 -0500

    + getOrdersByAccountId(Class<T>, Long> : Flux<T>

--- liquidPoolMarkets/src/main/java/liquidPoolOrder/IOrderRepository.java
+++ liquidPoolMarkets/src/main/java/liquidPoolOrder/IOrderRepository.java
@@ -2,17 +2,20 @@ package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolOrder;
 
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
+import reactor.core.publisher.Flux;
+
 /**
  * Persistence layer for Liquid Order entities.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.1
+ * @version 0.0.2
  */
 public interface IOrderRepository extends R2dbcRepository<LiquidOrder, Long>{
 	/*
 	 *  -- Read --
 	 */
+	<T extends ILiquidOrder> Flux<T> getOrdersByAccountId(Class<T> type, Long accountId);
 	
 	/*
 	 *  -- Update --
