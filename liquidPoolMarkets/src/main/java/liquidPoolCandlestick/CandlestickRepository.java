030919ded7747890d691e48c148b5f6be4def35e
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-20 23:15:52 -0500

    Swap entity

--- liquidPoolMarkets/src/main/java/liquidPoolCandlestick/CandlestickRepository.java
+++ liquidPoolMarkets/src/main/java/liquidPoolCandlestick/CandlestickRepository.java
@@ -2,13 +2,15 @@ package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolCandlestick;
 
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
+import reactor.core.publisher.Flux;
+
 /**
  * Persistence layer for Liquid Candlestick entities.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.2
+ * @version 0.0.3
  */
-public interface CandlestickRepository extends R2dbcRepository<Candlestick, Long>{
+public interface CandlestickRepository extends R2dbcRepository<DTOCandlestick, Long>{
 
 }
