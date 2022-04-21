6dbbd87c5e8229f041bd63352e290bb17c11186a
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-21 16:54:38 -0500

    + save(DTOCandlestick) : Mono<DTOCandlestick>

--- liquidPoolMarkets/src/main/java/liquidPoolCandlestick/ICandlestickService.java
+++ liquidPoolMarkets/src/main/java/liquidPoolCandlestick/ICandlestickService.java
@@ -3,16 +3,19 @@ package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolCandlestick;
 import org.springframework.stereotype.Service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * The user of this interface should implement the various actions associated
  * with an liquid pool candlestick service.
  * 
  * @author DZ-FSDev
- * @version 0.0.1
+ * @version 0.0.2
  * @since 17.0.2
  */
 @Service
 public interface ICandlestickService {
 	Flux<DTOCandlestick> getCandlesticksByMarketId(Long marketId);
+	
+	Mono<DTOCandlestick> save(DTOCandlestick candle);
 }
