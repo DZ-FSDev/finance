336840cb01de1d7c0b181b66a6e4f3df7bf0b8a6
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-22 12:24:09 -0500

    + placeOrder(Long, Long, BigInt) : Mono<DTOLiquidOrder>

--- liquidPoolMarkets/src/main/java/liquidAccountExchange/RESTController_v1_liquid_exchange_accounts.java
+++ liquidPoolMarkets/src/main/java/liquidAccountExchange/RESTController_v1_liquid_exchange_accounts.java
@@ -1,12 +1,14 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidAccountExchange;
 
+import java.math.BigInteger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import reactor.core.publisher.Flux;
+import com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolOrder.DTOLiquidOrder;
+
 import reactor.core.publisher.Mono;
 
 /**
@@ -14,11 +16,16 @@ import reactor.core.publisher.Mono;
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.1
+ * @version 0.0.2
  */
 @RestController
 @RequestMapping("/v1/liquid/exchange/accounts")
 public class RESTController_v1_liquid_exchange_accounts {
 	@Autowired
 	IAccountExchangeService svc;
+	
+	@GetMapping("/placeOrder")
+	public Mono<DTOLiquidOrder> placeOrder(Long accountId, Long marketId, BigInteger units){
+		return svc.placeOrder(accountId, marketId, units);
+	}
 }
