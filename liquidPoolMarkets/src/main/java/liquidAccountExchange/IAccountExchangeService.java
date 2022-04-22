1f2d36f0b7d2ea2f4d3ef39c63f1629e9ec870f9
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-22 08:05:21 -0500

    Create IAccountExchangeService.java

---
+++ liquidPoolMarkets/src/main/java/liquidAccountExchange/IAccountExchangeService.java
@@ -0,0 +1,18 @@
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidAccountExchange;
+
+import org.springframework.stereotype.Service;
+
+import reactor.core.publisher.Flux;
+
+/**
+ * The user of this interface should implement the various actions associated
+ * with a liquid pool account exchange services.
+ * 
+ * @author DZ-FSDev
+ * @version 0.0.1
+ * @since 17.0.1
+ */
+@Service
+public interface IAccountExchangeService {
+
+}
