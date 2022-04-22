f1dd475867bbc3c170b92032eb8813560247f3cd
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-22 08:06:55 -0500

    Create Trade.java

---
+++ liquidPoolMarkets/src/main/java/liquidPoolTrade/Trade.java
@@ -0,0 +1,29 @@
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolTrade;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Represents an Trade executed in a liquid pool market.
+ * 
+ * @author DZ-FSDev
+ * @since 17.0.2
+ * @version 0.0.1
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Trade implements ITrade{
+	private @Id Long id;
+}
