90095c2c346b56612d38d14e30c339364ddf1b6c
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-22 08:11:50 -0500

    Create ITradeService.java

---
+++ liquidPoolMarkets/src/main/java/liquidPoolTrade/ITradeService.java
@@ -0,0 +1,17 @@
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolTrade;
+
+import org.springframework.stereotype.Service;
+
+import reactor.core.publisher.Flux;
+
+/**
+ * The user of this interface should implement the various actions associated
+ * with a liquid pool trade services.
+ * 
+ * @author DZ-FSDev
+ * @version 0.0.1
+ * @since 17.0.1
+ */
+@Service
+public interface ITradeService {
+}
