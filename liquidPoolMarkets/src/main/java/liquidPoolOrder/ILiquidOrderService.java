361fa848cac79903da082a37f716920a4aff79f0
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-22 18:34:44 -0500

    + getOrdersByAccountIdAndStatus : Flux<T>

--- liquidPoolMarkets/src/main/java/liquidPoolOrder/ILiquidOrderService.java
+++ liquidPoolMarkets/src/main/java/liquidPoolOrder/ILiquidOrderService.java
@@ -9,11 +9,13 @@ import reactor.core.publisher.Flux;
  * with a liquid pool order services.
  * 
  * @author DZ-FSDev
- * @version 0.0.3
+ * @version 0.0.4
  * @since 17.0.1
  */
 @Service
 public interface ILiquidOrderService {
 	<T  extends ILiquidOrder> Flux<T> findByMarketId(Class<T> type, Long marketId);
 	<T  extends ILiquidOrder> Flux<T> findByAccountId(Class<T> type, Long accountId);
+	
+	<T extends ILiquidOrder> Flux<T> getOrdersByAccountIdAndStatus(Class<T> type, Long accountId, OrderStatus status);
 }
