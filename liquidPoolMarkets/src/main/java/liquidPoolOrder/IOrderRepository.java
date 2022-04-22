bd7ede547e8608795030ff2420f5a0c32fa427e7
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-22 18:27:09 -0500

    + findOrdersByMarketIdAndStatusOrderByTs() & getOrdersByMarketIdAndStatusOrderByTs()

--- liquidPoolMarkets/src/main/java/liquidPoolOrder/IOrderRepository.java
+++ liquidPoolMarkets/src/main/java/liquidPoolOrder/IOrderRepository.java
@@ -3,13 +3,14 @@ package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolOrder;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Persistence layer for Liquid Order entities.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.6
+ * @version 0.0.7
  */
 public interface IOrderRepository extends R2dbcRepository<DTOLiquidOrder, Long>{
 	/*
@@ -19,7 +20,8 @@ public interface IOrderRepository extends R2dbcRepository<DTOLiquidOrder, Long>{
 	<T extends ILiquidOrder> Flux<T> getOrdersByMarketIdOrderByTs(Class<T> type, Long marketId);
 	
 	<T extends ILiquidOrder> Flux<T> getOrdersByAccountIdAndStatus(Class<T> type, Long accountId, OrderStatus status);
-	<T extends ILiquidOrder> Flux<T> getOrdersByMarketIdAndStatusOrderByTs(Class<T> type, Long marketId, OrderStatus status);
+	<T extends ILiquidOrder> Mono<T> getOrdersByMarketIdAndStatusOrderByTs(Class<T> type, Long marketId, OrderStatus status);
+	<T extends ILiquidOrder> Flux<T> findOrdersByMarketIdAndStatusOrderByTs(Class<T> type, Long marketId, OrderStatus status);
 	/*
 	 *  -- Update --
 	 */
