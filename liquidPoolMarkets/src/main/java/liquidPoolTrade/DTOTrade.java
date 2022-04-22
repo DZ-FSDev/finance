bfc0fccf5960ddd0b46a356ea068fc8b6936a0fa
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-22 08:08:36 -0500

    Create DTOTrade.java

---
+++ liquidPoolMarkets/src/main/java/liquidPoolTrade/DTOTrade.java
@@ -0,0 +1,27 @@
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolTrade;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Represents an Trade executed in a liquid pool market.
+ * 
+ * @author DZ-FSDev
+ * @since 17.0.2
+ * @version 0.0.1
+ */
+@Data
+@Table("trade")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class DTOTrade implements ITrade{
+	private @Id Long id;
+}
