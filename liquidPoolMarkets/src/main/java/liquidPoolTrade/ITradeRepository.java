8e28a39c824571570cbfff69693fbbc97bbeb8ce
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-22 08:09:43 -0500

    Create ITradeRepository.java

---
+++ liquidPoolMarkets/src/main/java/liquidPoolTrade/ITradeRepository.java
@@ -0,0 +1,22 @@
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolTrade;
+
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+import reactor.core.publisher.Flux;
+
+/**
+ * Persistence layer for Liquid Trade entities.
+ * 
+ * @author DZ-FSDev
+ * @since 17.0.2
+ * @version 0.0.1
+ */
+public interface ITradeRepository extends R2dbcRepository<DTOTrade, Long>{
+	/*
+	 *  -- Read --
+	 */
+	
+	/*
+	 *  -- Delete --
+	 */
+}
