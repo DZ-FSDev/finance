a69ec3e14c1de49c33d8b5548ab469335747f87c
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-23 13:36:27 -0500

    Create DTOMarket.java

---
+++ liquidPoolMarkets/src/main/java/liquidPoolMarket/DTOMarket.java
@@ -0,0 +1,50 @@
+/*  Original Licensing Copyright
+ * 
+ *  Represents a liquid pool market.
+ *  Copyright (C) 2022  DZ-FSDev
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolMarket;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Represents a liquid pool market.
+ * 
+ * @author DZ-FSDev
+ * @since 17.0.2
+ * @version 0.0.1
+ */
+@Table("market")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class DTOMarket implements IMarket{
+	private @Id Long id;
+	
+	private String ticker;
+
+	private Long leftAssetId;
+	private Long rightAssetId;
+}
