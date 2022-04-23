e0155da43148ff7da0e268b01c6d1eacd1712ff8
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-22 23:17:13 -0500

    Flux => Mono

--- liquidPoolMarkets/src/main/java/liquidPoolPosition/IPositionRepository.java
+++ liquidPoolMarkets/src/main/java/liquidPoolPosition/IPositionRepository.java
@@ -3,13 +3,14 @@ package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolPosition;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Persistence layer for Liquid Position entities.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.7
+ * @version 0.0.8
  */
 public interface IPositionRepository extends R2dbcRepository<DTOPosition, Long>{
 	/*
@@ -18,7 +19,7 @@ public interface IPositionRepository extends R2dbcRepository<DTOPosition, Long>{
 	<T extends IPosition> Flux<T> findAllBy(Class<T> type);
 	<T extends IPosition> Flux<T> findByAccountId(Class<T> type, Long accountId);
 	
-	<T extends IPosition> Flux<T> findByAccountIdAndAssetId(Class<T> type, Long accountId, Long assetId);
+	<T extends IPosition> Mono<T> findByAccountIdAndAssetId(Class<T> type, Long accountId, Long assetId);
 	/*
 	 *  -- Delete --
 	 */
