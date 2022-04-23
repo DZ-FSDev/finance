58eee0a5980d40e6d30642f4e09043e5359b738f
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-23 14:51:35 -0500

    Update EntityByIdRequest.java

--- liquidPoolMarkets/src/main/java/liquidAccountExchange/EntityByIdRequest.java
+++ liquidPoolMarkets/src/main/java/liquidAccountExchange/EntityByIdRequest.java
@@ -1,5 +1,6 @@
 package com.dz_fs_dev.finance.liquidPoolMarkets.liquidAccountExchange;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
@@ -8,9 +9,10 @@ import lombok.Data;
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.1
+ * @version 0.0.2
  */
 @Data
+@AllArgsConstructor
 public class EntityByIdRequest {
 	private Long id;
 	Class<?> type;
