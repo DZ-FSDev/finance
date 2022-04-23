b4f61224c29758ee085ce03ea8f94963b0ad2989
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-22 19:00:48 -0500

    + doc templating

--- liquidPoolMarkets/src/main/java/liquidPoolOrder/ILiquidOrderService.java
+++ liquidPoolMarkets/src/main/java/liquidPoolOrder/ILiquidOrderService.java
@@ -3,6 +3,7 @@ package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolOrder;
 import org.springframework.stereotype.Service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * The user of this interface should implement the various actions associated
@@ -14,10 +15,56 @@ import reactor.core.publisher.Flux;
  */
 @Service
 public interface ILiquidOrderService {
-	<T  extends ILiquidOrder> Flux<T> findByMarketId(Class<T> type, Long marketId);
-	<T  extends ILiquidOrder> Flux<T> findByAccountId(Class<T> type, Long accountId);
+	/**
+	 * 
+	 * @param <T>
+	 * @param type
+	 * @param marketId
+	 * @return
+	 * @since 0.0.3
+	 */
+	<T extends ILiquidOrder> Flux<T> findByMarketId(Class<T> type, Long marketId);
 	
+	/**
+	 * 
+	 * @param <T>
+	 * @param type
+	 * @param accountId
+	 * @return
+	 * @since 0.0.3
+	 */
+	<T extends ILiquidOrder> Flux<T> findByAccountId(Class<T> type, Long accountId);
+	
+	/**
+	 * 
+	 * @param <T>
+	 * @param type
+	 * @param accountId
+	 * @param status
+	 * @return
+	 * @since 0.0.4
+	 */
 	<T extends ILiquidOrder> Flux<T> getOrdersByAccountIdAndStatus(Class<T> type, Long accountId, OrderStatus status);
+	
+	/**
+	 * 
+	 * @param <T>
+	 * @param type
+	 * @param marketId
+	 * @param status
+	 * @return
+	 * @since 0.0.5
+	 */
 	<T extends ILiquidOrder> Mono<T> getOrdersByMarketIdAndStatusOrderByTs(Class<T> type, Long marketId, OrderStatus status);
+	
+	/**
+	 * 
+	 * @param <T>
+	 * @param type
+	 * @param marketId
+	 * @param status
+	 * @return
+	 * @since 0.0.5
+	 */
 	<T extends ILiquidOrder> Flux<T> findOrdersByMarketIdAndStatusOrderByTs(Class<T> type, Long marketId, OrderStatus status);
 }
