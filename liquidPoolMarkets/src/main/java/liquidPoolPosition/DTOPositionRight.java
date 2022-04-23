9287104c388dfa651c63afadee8604e2ee6e4023
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-23 16:16:14 -0500

    Create DTOPositionRight.java

---
+++ liquidPoolMarkets/src/main/java/liquidPoolPosition/DTOPositionRight.java
@@ -0,0 +1,33 @@
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolPosition;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Represents a liquid pool position.
+ * 
+ * @author DZ-FSDev
+ * @since 17.0.2
+ * @version 0.0.1
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class DTOPositionRight implements IPosition{
+	private @Id Long id;
+
+	private String units;
+	private String lockedUnits;
+
+	private @Transient String unrealizedPNL;
+	
+	private Long accountId;
+	private Long assetId;
+}
