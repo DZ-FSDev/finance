8f1e7b624d591a06c18af9a947202cf879747cfa
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-23 15:30:04 -0500

    Create DTOAssetRight.java

---
+++ liquidPoolMarkets/src/main/java/liquidPoolAsset/DTOAssetRight.java
@@ -0,0 +1,31 @@
+package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolAsset;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Represents the right Asset in liquid pool markets.
+ * 
+ * @author DZ-FSDev
+ * @version 0.0.1
+ * @since 17.0.1
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class DTOAssetRight implements IAsset{
+	private @Id Long id;
+
+	private String name;
+
+	private String units;
+	private AssetClass assetClass;
+}
