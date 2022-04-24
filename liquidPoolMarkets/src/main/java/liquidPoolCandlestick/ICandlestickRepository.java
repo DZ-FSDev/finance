d9229b8966bfdde012f48f10cbb4a03923ef21af
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-24 12:58:12 -0500

    + getByMarketIdOrderByOpenTSDesc() : Flux<T>

--- liquidPoolMarkets/src/main/java/liquidPoolCandlestick/ICandlestickRepository.java
+++ liquidPoolMarkets/src/main/java/liquidPoolCandlestick/ICandlestickRepository.java
@@ -3,14 +3,16 @@ package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolCandlestick;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Persistence layer for Liquid Candlestick entities.
  * 
  * @author DZ-FSDev
  * @since 17.0.2
- * @version 0.0.6
+ * @version 0.0.7
  */
 public interface ICandlestickRepository extends R2dbcRepository<DTOCandlestick, Long>{
 	<T extends ICandlestick> Flux<T> findByMarketIdOrderByOpenTS(Class<T> type, Long marketId);
+	<T extends ICandlestick> Mono<T> getByMarketIdOrderByOpenTSDesc(Class<T> type, Long marketId);
 }
