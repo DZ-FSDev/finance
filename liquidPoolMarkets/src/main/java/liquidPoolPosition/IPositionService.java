cfa8829544f4e65b373837736a3d4d806fac43eb
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-04-25 21:20:25 -0500

    + findByAccountId(Class<T>, Long) : Flux<T>

--- liquidPoolMarkets/src/main/java/liquidPoolPosition/IPositionService.java
+++ liquidPoolMarkets/src/main/java/liquidPoolPosition/IPositionService.java
@@ -2,15 +2,25 @@ package com.dz_fs_dev.finance.liquidPoolMarkets.liquidPoolPosition;
 
 import org.springframework.stereotype.Service;
 
+import reactor.core.publisher.Flux;
+
 /**
  * The user of this interface should implement the various actions associated
  * with a liquid pool position services.
  * 
  * @author DZ-FSDev
- * @version 0.0.1
+ * @version 0.0.2
  * @since 17.0.1
  */
 @Service
 public interface IPositionService {
-
+	/**
+	 * 
+	 * @param <T>
+	 * @param type
+	 * @param id
+	 * @return
+	 * @since 0.0.2
+	 */
+	<T extends IPosition> Flux<T> findByAccountId(Class<T> type, Long id);
 }
