f3e2e654a141fd421be0543ea7ebee3b9f4b1ef4
Don Zhu <85122787+DZ-FSDev@users.noreply.github.com>
2022-05-13 15:36:07 -0500

    Update Levels.java

--- analytics/Levels.java
+++ analytics/Levels.java
@@ -5,11 +5,12 @@ import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 
 /**
- * 
+ * Utility class providing functionality for calculating different price
+ * levels.
  * 
  * @author DZ-FSDev
  * @since 17.0.1
- * @version 0.0.1
+ * @version 0.0.2
  */
 public final class Levels {
 	private Levels() {}
@@ -19,6 +20,25 @@ public final class Levels {
 		p5(new BigDecimal("0.5"));
 
 		final BigDecimal fib;
+		
+		/**
+		 * 
+		 * @param price
+		 * @return
+		 */
+		BigDecimal apply(BigDecimal price){
+			return price.multiply(fib);
+		}
+		
+		/**
+		 * 
+		 * @param support
+		 * @param resistance
+		 * @return
+		 */
+		BigDecimal apply(BigDecimal support, BigDecimal resistance){
+			BigDecimal delta = resistance.subtract(support);
+			return support.add(delta.multiply(fib));
+		}
 	}
-	
 }
